7f8f679cd3ee5c039a6fb59ac299cf94f46c59c2
Harish H U <harishssbj36@gmail.com>
2025-08-21 19:10:53 +0530

    Add files via upload

---
+++ Drum Kit mini Project/index.js
@@ -0,0 +1,49 @@
+let drums=document.querySelectorAll(".drum");
+
+drums.forEach((drum)=>{
+    drum.addEventListener("click", ()=>{
+    var buttonInnerHTML=drum.innerHTML;
+    makeSound(buttonInnerHTML);
+
+    });
+}); 
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+});
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1=new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;    
+        case "s":
+            var tom3=new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash=new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kickBass=new Audio("./sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        case "l":
+            var l=new Audio("./sounds/snare.mp3");
+            l.play();
+            break;
+        default:console.log(buttonInnerHTML);
+            break;
+    }
+}
